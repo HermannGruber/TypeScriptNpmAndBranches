0f85b1de6dd12da16bd94e2dad3ae543ae1653d3
Hermann Gruber <HermannGruber@users.noreply.github.com>
2019-04-16 11:23:56 +0200

    add webpack.config.js

---
+++ WebApplication1/webpack.config.js
@@ -0,0 +1,27 @@
+﻿const path = require('path');
+
+module.exports = {
+    entry: {
+        hello: './scripts/hello.ts'
+    },
+    mode: 'production',
+    optimization: {
+        minimize: false
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'wwwroot/js'),
+    }
+};
