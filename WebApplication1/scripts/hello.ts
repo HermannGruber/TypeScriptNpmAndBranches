0cb655619aae7cca13d9f48fd7f6c9c47eadf583
Hermann Gruber <HermannGruber@users.noreply.github.com>
2019-04-16 12:17:36 +0200

    hello.ts changed to use modules

--- WebApplication1/scripts/hello.ts
+++ WebApplication1/scripts/hello.ts
@@ -1,5 +1,5 @@
-﻿//import * as $ from "jquery"
-declare var $ : any;
+﻿import * as $ from "jquery"
+//declare var $ : any;
 $(() => {
     document.body.innerHTML += "<i>Hello TypeScript using jQuery</i>";
 });
